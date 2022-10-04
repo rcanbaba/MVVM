93a1afc4f9b1523bdd76dd17bba6a99c7d0ba070
Can Babaoğlu <rcanbabaoglu@gmail.com>
2022-10-04 20:19:19 +0300

    MVVM-2-MovieApp initial settings.

---
+++ MVVM-2-MovieApp/MVVM-2-MovieApp/MVVM-2-MovieApp/Controller/MovieHomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MovieHomeViewController.swift
+//  MVVM-2-MovieApp
+//
+//  Created by Can Babaoğlu on 4.10.2022.
+//
+
+import UIKit
+
+class MovieHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+}
