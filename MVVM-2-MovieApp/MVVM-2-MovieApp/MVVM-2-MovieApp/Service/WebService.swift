411545e8e2b48ef434e622b0d7b40b39227aff31
Can Babaoğlu <rcanbabaoglu@gmail.com>
2022-10-05 00:08:06 +0300

    service layer implemented.

---
+++ MVVM-2-MovieApp/MVVM-2-MovieApp/MVVM-2-MovieApp/Service/WebService.swift
@@ -0,0 +1,23 @@
+//
+//  WebService.swift
+//  MVVM-2-MovieApp
+//
+//  Created by Can Babaoğlu on 4.10.2022.
+//
+
+import Alamofire
+
+protocol ServiceProtocol {
+    func fetchMovies(onSuccess: @escaping (Movies?) -> (), onError: @escaping (AFError) -> ())
+}
+
+final class Service: ServiceProtocol {
+    func fetchMovies(onSuccess: @escaping (Movies?) -> (), onError: @escaping (AFError) -> ()) {
+        ServiceManager.shared.fetch(path: Constant.ServiceEndpoint.fetchMovies()) { (response: Movies) in
+            onSuccess(response)
+        } onError: { error in
+            onError(error)
+        }
+
+    }
+}
