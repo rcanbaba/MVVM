78bc16172b3ee3c94ac04a6e48b1f8bc04abdaad
Can Babaoğlu <rcanbabaoglu@gmail.com>
2022-10-15 16:04:55 +0300

    compositional layout implemented.

--- MVVM-2-MovieApp/MVVM-2-MovieApp/MVVM-2-MovieApp/Controller/MovieHomeViewController.swift
+++ MVVM-2-MovieApp/MVVM-2-MovieApp/MVVM-2-MovieApp/Controller/MovieHomeViewController.swift
@@ -11,25 +11,19 @@ import SnapKit
 final class MovieHomeViewController: UIViewController {
     
     //TODO: add view components a new UIView class then add here (MovieHomeViewController)
-    private lazy var tableView: UITableView = {
-        let tableView = UITableView()
-        //tableView.register(RMTableViewCell.self, forCellReuseIdentifier: RMTableViewCell.reuseIdentifier)
-        tableView.showsVerticalScrollIndicator = false
-        tableView.showsHorizontalScrollIndicator = false
-        //tableView.rowHeight = 120
-        tableView.backgroundColor = .lightGray
-        tableView.separatorStyle = .none
-        //TODO: if needed implement
-        tableView.refreshControl = UIRefreshControl()
-        tableView.refreshControl?.tintColor = .white
-        tableView.layer.cornerRadius = 10.0
-        return tableView
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: getCompositionalLayout())
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .lightGray
+        collectionView.register(MovieCollectionViewCell.self, forCellWithReuseIdentifier: MovieCollectionViewCell.reuseIdentifier)
+        return collectionView
     }()
     
     private lazy var titleLabel: UILabel = {
         var label = UILabel()
         label.numberOfLines = 1
-        label.textColor = .white
+        label.textColor = .black
         label.textAlignment = .center
         label.font = UIFont.systemFont(ofSize: 22.0, weight: .bold)
         label.text = "Movies"
@@ -44,8 +38,6 @@ final class MovieHomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .gray
-        tableView.delegate = self
-        tableView.dataSource = self
         configureUI()
         getData()
     }
@@ -55,12 +47,45 @@ final class MovieHomeViewController: UIViewController {
         setTableView()
     }
     
+    private func getCompositionalLayout() -> UICollectionViewCompositionalLayout {
+        // fractionalWidth -> corresponding main width
+        
+        let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1/3)))
+        item.contentInsets = NSDirectionalEdgeInsets(top: 1, leading: 1, bottom: 1, trailing: 1)
+        
+        // ---- Group 1 ----
+        let group1Item1 = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1/2), heightDimension: .fractionalHeight(1)))
+        group1Item1.contentInsets = NSDirectionalEdgeInsets(top: 1, leading: 1, bottom: 1, trailing: 1)
+        
+        let nestedGroup1Item1 = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1/2)))
+        nestedGroup1Item1.contentInsets = NSDirectionalEdgeInsets(top: 1, leading: 1, bottom: 1, trailing: 1)
+        
+        let nestedGroup1 = NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1/2), heightDimension: .fractionalHeight(1)), subitems: [nestedGroup1Item1])
+        
+        
+        let group1 = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1/3)), subitems: [group1Item1, nestedGroup1])
+        
+        // ---- Group 2 ----
+        let group2Item1 = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1/3), heightDimension: .fractionalHeight(1)))
+        group2Item1.contentInsets = NSDirectionalEdgeInsets(top: 1, leading: 1, bottom: 1, trailing: 1)
+        let group2 = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1/3)), subitems: [group2Item1])
+        
+        // ---- Container Group ----
+        let containerGroupLayoutSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(600))
+        let containerGroup = NSCollectionLayoutGroup.vertical(layoutSize: containerGroupLayoutSize, subitems: [item, group1, group2])
+        
+        let section = NSCollectionLayoutSection(group: containerGroup)
+        let compositionalLayout = UICollectionViewCompositionalLayout(section: section)
+        
+        return compositionalLayout
+    }
+    
     private func getData() {
         viewModel.fetchMovies { [weak self] (response) in
             if let results = response?.results {
                 self?.searchResults = results
                 DispatchQueue.main.async {
-                    self?.tableView.reloadData()
+                    self?.collectionView.reloadData()
                 }
             }
         }
@@ -69,27 +94,57 @@ final class MovieHomeViewController: UIViewController {
     private func setTitleLabel() {
         view.addSubview(titleLabel)
         titleLabel.snp.makeConstraints { (make) in
-            make.top.leading.trailing.equalToSuperview().inset(8)
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().inset(60)
         }
     }
     
     private func setTableView() {
-        view.addSubview(tableView)
-        tableView.snp.makeConstraints { (make) in
-            make.top.equalTo(titleLabel.snp.bottom).offset(10)
-            make.leading.trailing.bottom.equalToSuperview().inset(10)
+        view.addSubview(collectionView)
+        collectionView.snp.makeConstraints { (make) in
+            make.top.equalTo(titleLabel.snp.bottom).offset(16)
+            make.leading.trailing.equalToSuperview().inset(12)
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
         }
     }
 }
 
-extension MovieHomeViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+extension MovieHomeViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MovieCollectionViewCell.reuseIdentifier, for: indexPath) as! MovieCollectionViewCell
+        cell.backgroundColor = .systemPurple
+        
+        let cellData = searchResults[indexPath.row]
+        cell.setImage(cellData.posterPath ?? "")
+        cell.setName(cellData.title ?? "unknown")
+        return cell
+    }
+}
+
+extension MovieHomeViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return searchResults.count
     }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = searchResults[indexPath.item].title ?? "unknown"
-        return cell
+}
+
+//
+//extension MovieHomeViewController: UITableViewDelegate, UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return searchResults.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = UITableViewCell()
+//        cell.textLabel?.text = searchResults[indexPath.item].title ?? "unknown"
+//        return cell
+//    }
+//}
+
+
+extension UIView {
+    // collection view and table view cells identifier
+    static var reuseIdentifier: String {
+        return String(describing: self)
     }
 }
