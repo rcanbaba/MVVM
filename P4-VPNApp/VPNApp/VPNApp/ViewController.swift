21d316cf9a7640054c01eceb902349064cd8a374
rcanbaba <rcanbabaoglu@gmail.com>
2022-11-30 22:09:43 +0300

    started.

--- P4-VPNApp/VPNApp/VPNApp/ViewController.swift
+++ P4-VPNApp/VPNApp/VPNApp/ViewController.swift
@@ -6,15 +6,40 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    private lazy var emptyButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .green
+        button.addTarget(self, action: #selector(onButtonTap(_:)), for: .touchUpInside)
+        button.layer.cornerRadius = 10.0
+        button.setTitle("Connect", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.blue
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.gray
+        
+        configureUI()
     }
 
+    private func configureUI() {
+        view.addSubview(emptyButton)
+        emptyButton.snp.makeConstraints { (make) in
+            make.width.equalTo(100)
+            make.height.equalTo(60)
+            make.center.equalToSuperview()
+        }
+    }
 
+    
+    @objc private func onButtonTap(_ sender: Any) {
+        print("Button Tapped")
+    }
+    
 }
 
