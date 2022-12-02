3bc54bf9afab62aaac33e0669a23d314916dd9b0
rcanbaba <rcanbabaoglu@gmail.com>
2022-12-02 23:30:51 +0300

    P5 photoDetailVC optimized.

--- P5-GCD-tutorial/GooglyPuff/PhotoDetailViewController.swift
+++ P5-GCD-tutorial/GooglyPuff/PhotoDetailViewController.swift
@@ -33,53 +33,65 @@
 import UIKit
 
 private enum ScaleFactor {
-  static let retinaToEye: CGFloat = 0.5
-  static let faceBoundsToEye: CGFloat = 4.0
+    static let retinaToEye: CGFloat = 0.5
+    static let faceBoundsToEye: CGFloat = 4.0
 }
 
 final class PhotoDetailViewController: UIViewController {
-  @IBOutlet weak var photoImageView: UIImageView!
-
-  var image: UIImage?
-
-  // MARK: - Lifecycle
-  override func viewDidLoad() {
-    super.viewDidLoad()
-
-    assert(image != nil, "Image not set; required to use view controller")
-    guard let image = image else {
-      return
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    var image: UIImage?
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        assert(image != nil, "Image not set; required to use view controller")
+        guard let image = image else {
+            return
+        }
+        photoImageView.image = image
+        
+        // Resize if necessary to ensure it's not pixelated
+        if image.size.height <= photoImageView.bounds.size.height &&
+            image.size.width <= photoImageView.bounds.size.width {
+            photoImageView.contentMode = .center
+        }
+        
+        // NOTE: [weak self] to capture a weak reference to self in each closure.
+        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+            guard let overlayImage = image.faceOverlayImageFrom() else {
+                return
+            }
+            
+            // NOTE: anything that modifies the UI must run on the main thread!
+            DispatchQueue.main.async { [weak self] in
+                self?.fadeInNewImage(overlayImage)
+            }
+        }
+        // NOTE: In general, you want to use async when you need to perform a network-based or CPU-intensive task in the background without blocking the current thread
+        
+        // ÖZET:
+        // eskiden controllerı açmayı ve gözleri ayarlamayı aynı anda yapıp, controller ı açmak için bizi bekletiyordu.
+        // şimdi controllerı direkt açıyor sonrasında gözleri koyuyor.
     }
-    photoImageView.image = image
-
-    // Resize if necessary to ensure it's not pixelated
-    if image.size.height <= photoImageView.bounds.size.height &&
-      image.size.width <= photoImageView.bounds.size.width {
-      photoImageView.contentMode = .center
-    }
-
-    guard let overlayImage = image.faceOverlayImageFrom() else {
-      return
-    }
-    fadeInNewImage(overlayImage)
-  }
 }
 
 // MARK: - Private Methods
 private extension PhotoDetailViewController {
-  func fadeInNewImage(_ newImage: UIImage) {
-    let tmpImageView = UIImageView(image: newImage)
-    tmpImageView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-    tmpImageView.contentMode = photoImageView.contentMode
-    tmpImageView.frame = photoImageView.bounds
-    tmpImageView.alpha = 0.0
-    photoImageView.addSubview(tmpImageView)
-
-    UIView.animate(withDuration: 0.75, animations: {
-      tmpImageView.alpha = 1.0
-    }, completion: { _ in
-      self.photoImageView.image = newImage
-      tmpImageView.removeFromSuperview()
-    })
-  }
+    func fadeInNewImage(_ newImage: UIImage) {
+        let tmpImageView = UIImageView(image: newImage)
+        tmpImageView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        tmpImageView.contentMode = photoImageView.contentMode
+        tmpImageView.frame = photoImageView.bounds
+        tmpImageView.alpha = 0.0
+        photoImageView.addSubview(tmpImageView)
+        
+        UIView.animate(withDuration: 0.75, animations: {
+            tmpImageView.alpha = 1.0
+        }, completion: { _ in
+            self.photoImageView.image = newImage
+            tmpImageView.removeFromSuperview()
+        })
+    }
 }
