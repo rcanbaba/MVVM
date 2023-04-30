abfd3f64433c1d86bc04739aeb44974c8b8a5ad6
rcanbaba <rcanbabaoglu@gmail.com>
2023-04-30 18:11:08 +0300

    P6-WeSplit-SwiftUI added.

---
+++ P6-WeSplit-SwiftUI/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Can Babaoğlu on 24.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
